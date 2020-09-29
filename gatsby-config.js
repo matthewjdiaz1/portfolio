009d0a3d60126378b12e852ba186607004f7c682
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-09-29 15:57:35 -0700

    added profile info

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of Matthew J Diaz",
+    locale: "en",
+    title: "Matthew J Diaz",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "dark-blue",
+        showThemeLogo: false,
+      },
+    },
+  ],
+}
